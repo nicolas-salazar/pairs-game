7ba43a3e949b2861634e9fb6350a24a85665bed7
Nicolás Salazar <nicolas.salazar@ncodes.co>
2020-04-03 16:24:35 -0500

    Se ajustan los tamanios de fuente a disenio responsivo

--- src/views/components/gameAuxiliars/Card.jsx
+++ src/views/components/gameAuxiliars/Card.jsx
@@ -18,23 +18,33 @@ class Card extends React.Component {
 
     render() {
         return (
-            <TweenOne animation={{
-                ...animation, duration: (1000 / 7) * 2,
-                bezier: {
-                    ...animation.bezier, vars: [{
-                        x: this.props.xIndex * (this.props.cardSize.width) + 10,
-                        y: this.props.yIndex * (this.props.cardSize.width / 0.76) + 10
-                    }]
-                },
-            }}>
-                <img key={""} alt="" src={"https://deckofcardsapi.com/static/img/QS.png"}
-                    style={{
-                        width: this.props.cardSize.width,
-                        height: (this.props.cardSize.width / 0.76),
-                        position: "absolute", left: "0%", top: "0%",
-                        paddingLeft: 5, paddingRight: 5, paddingTop: 5, paddingBottom: 5,
-                    }} />
-            </TweenOne>
+            // <TweenOne animation={{
+            //     ...animation, duration: (1000 / 7) * 2,
+            //     bezier: {
+            //         ...animation.bezier, vars: [{
+            //             x: this.props.xIndex * (this.props.cardSize.width) + 10,
+            //             y: this.props.yIndex * (this.props.cardSize.width / 0.75) + 10
+            //         }]
+            //     },
+            // }}>
+            //     <img key={""} alt="" src={"https://deckofcardsapi.com/static/img/QS.png"}
+            //         style={{
+            //             width: this.props.cardSize.width,
+            //             height: (this.props.cardSize.width / 0.75),
+            //             position: "absolute", left: "0%", top: "0%",
+            //             paddingLeft: 5, paddingRight: 5, paddingTop: 5, paddingBottom: 5,
+            //         }} />
+            // </TweenOne>
+
+            <img key={""} alt="" src={"https://deckofcardsapi.com/static/img/QS.png"}
+                style={{
+                    width: this.props.cardSize.width,
+                    height: (this.props.cardSize.width / 0.75),
+                    position: "absolute",
+                    left: this.props.xIndex * (this.props.cardSize.width) + 0,
+                    top: this.props.yIndex * (this.props.cardSize.width / 0.75) + 10,
+                    paddingLeft: 5, paddingRight: 5, paddingTop: 5, paddingBottom: 5,
+                }} />
         );
     }
 }
--- src/views/components/gameAuxiliars/CardsTable.jsx
+++ src/views/components/gameAuxiliars/CardsTable.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import PerfectScrollbar from 'react-perfect-scrollbar';
+
 //Componentes auxiliares:
 import Card from './Card';
 
@@ -16,29 +18,31 @@ let CardsTable = (props) => {
 
     return (
         <div style={{ padding: topPadding, paddingLeft: 0 }}>
-            <Card xIndex={0} yIndex={0} cardSize={cardSize}/>
-            <Card xIndex={1} yIndex={0} cardSize={cardSize}/>
-            <Card xIndex={2} yIndex={0} cardSize={cardSize}/>
-            <Card xIndex={3} yIndex={0} cardSize={cardSize}/>
-            <Card xIndex={4} yIndex={0} cardSize={cardSize}/>
+            <PerfectScrollbar style={{ maxHeight: avaliableHeight }}>
+                <Card xIndex={0} yIndex={0} cardSize={cardSize} />
+                <Card xIndex={1} yIndex={0} cardSize={cardSize} />
+                <Card xIndex={2} yIndex={0} cardSize={cardSize} />
+                <Card xIndex={3} yIndex={0} cardSize={cardSize} />
+                <Card xIndex={4} yIndex={0} cardSize={cardSize} />
 
-            <Card xIndex={0} yIndex={1} cardSize={cardSize}/>
-            <Card xIndex={1} yIndex={1} cardSize={cardSize}/>
-            <Card xIndex={2} yIndex={1} cardSize={cardSize}/>
-            <Card xIndex={3} yIndex={1} cardSize={cardSize}/>
-            <Card xIndex={4} yIndex={1} cardSize={cardSize}/>
+                <Card xIndex={0} yIndex={1} cardSize={cardSize} />
+                <Card xIndex={1} yIndex={1} cardSize={cardSize} />
+                <Card xIndex={2} yIndex={1} cardSize={cardSize} />
+                <Card xIndex={3} yIndex={1} cardSize={cardSize} />
+                <Card xIndex={4} yIndex={1} cardSize={cardSize} />
 
-            <Card xIndex={0} yIndex={2} cardSize={cardSize}/>
-            <Card xIndex={1} yIndex={2} cardSize={cardSize}/>
-            <Card xIndex={2} yIndex={2} cardSize={cardSize}/>
-            <Card xIndex={3} yIndex={2} cardSize={cardSize}/>
-            <Card xIndex={4} yIndex={2} cardSize={cardSize}/>
+                <Card xIndex={0} yIndex={2} cardSize={cardSize} />
+                <Card xIndex={1} yIndex={2} cardSize={cardSize} />
+                <Card xIndex={2} yIndex={2} cardSize={cardSize} />
+                <Card xIndex={3} yIndex={2} cardSize={cardSize} />
+                <Card xIndex={4} yIndex={2} cardSize={cardSize} />
 
-            <Card xIndex={0} yIndex={3} cardSize={cardSize}/>
-            <Card xIndex={1} yIndex={3} cardSize={cardSize}/>
-            <Card xIndex={2} yIndex={3} cardSize={cardSize}/>
-            <Card xIndex={3} yIndex={3} cardSize={cardSize}/>
-            <Card xIndex={4} yIndex={3} cardSize={cardSize}/>
+                <Card xIndex={0} yIndex={3} cardSize={cardSize} />
+                <Card xIndex={1} yIndex={3} cardSize={cardSize} />
+                <Card xIndex={2} yIndex={3} cardSize={cardSize} />
+                <Card xIndex={3} yIndex={3} cardSize={cardSize} />
+                <Card xIndex={4} yIndex={3} cardSize={cardSize} />
+            </PerfectScrollbar>
         </div>
     );
 }
